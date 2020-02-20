9ba4f28602f5f36befa9b40483866cf1c068dbb3
yuji nakamura <yuji@yujis-MacBook-Air.local>
2020-02-20 21:13:46 +0900

    added jQuery and modified HTML and CSS for responsive design

--- js/main.js
+++ js/main.js
@@ -4,15 +4,32 @@ AOS.init();
 // jQuery //
 $(function() {
 
+    // nav //
+    // menu listsの開閉。 //
+    $('#menu-toggle').on('click', function() {
+        $('.menu-bar ul').toggleClass('menu-lists-active');
+        $('body').toggleClass('fixed');
+    });
+
+    // menuのlistをクリックするとmenu-listsが閉じる。 //
+    $(".toggle-closer").on('click', function() {
+        $(".menu-bar ul").removeClass('menu-lists-active');
+        $('body').toggleClass('fixed');
+    });
+    // nav //
+ 
+
     /* modal for project item */
     // モーダル開く //
     $(".project-click-modal").on("click", function() {
         const projectTarget = "#" + $(this).attr('data-target');
         $(projectTarget).fadeIn();
+        $('body').toggleClass('fixed');
     });
     // モーダル閉じる //
     $("#close-project-modal, .project-modal").on("click", function() {
         $(".project-modal").fadeOut();
+        $('body').toggleClass('fixed');
     });
     // モーダルの白い部分をクリックしても閉じないよう。 //
     $('.project-modal-content').on('click', event => {
@@ -28,17 +45,6 @@ $(function() {
         $("#pj-main-img2").attr('src', $(this).attr('src'));
     });
     /* modal for project item */
-
-    // scroll position // 
-    var scrollPosition;
-    $('.project-click-modal').on('click', function() {
-        scrollPosition = $(window).scrollTop();
-        $('body').addClass('fixed').css({'top': -scrollPosition});
-    });
-    $('.close-project-modal').on('click', function() {
-        $('body').removeClass('fixed').css({'top': 0});
-        window.scrollTo( 0 , scrollPosition );
-    });
 
 });
 // jQuery //
